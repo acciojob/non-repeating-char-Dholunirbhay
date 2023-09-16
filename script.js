98b4190c607c4f7b82dec8961e5abd7a205a52f3
Dholunirbhay <102872061+Dholunirbhay@users.noreply.github.com>
2023-09-16 11:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+	for(let i=0 ; i<str.length() ; i++){
+		boolean isrepeated = false;
+		for(let j=0 ; j<str.length() ; j++){
+			if(str.charAt(i)==str.charAt(j)){
+				isrepeated = true;
+				break;
+			}
+		}
+		if(isrepeated == false){
+			return arr[i];
+		}
+	}
+	return ;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
